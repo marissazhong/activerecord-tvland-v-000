a7c05642a37638b4c660b092990f741e31225e69
Marissa Zhong <mwz2101@columbia.edu>
2018-07-10 14:08:48 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,8 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   def list_roles
-    characters
+    characters.map {|character|
+      "#{character.name} - #{character.show_id}"
+    }
   end
 end
